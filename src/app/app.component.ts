08694c016649e859f34f6ed13daa5fd2c05bb514
geeook <LiuMengzhou@users.noreply.github.com>
2017-04-20 09:32:41 +0800

    1. package.json增加了对material design的依赖 2. 初步完成静态首页，增加了全屏背景 3. 增加了图标

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  portraitPath = "assets/images/portrait.jpg";
+  myName = "Geeook";
+  description = "You Only Live Once";
+  navs = [
+    {title: "全部",
+      url: "",
+      img: "assets/images/list.png",
+      src: "assets/images/list.png",
+      srcHover: "assets/images/list-clicked.png"
+    },
+    {title: "分类",
+      url: "",
+      img: "assets/images/category.png",
+      src: "assets/images/category.png",
+      srcHover: "assets/images/category-clicked.png"
+    },
+    {title: "关于",
+      url: "",
+      img: "assets/images/about.png",
+      src: "assets/images/about.png",
+      srcHover: "assets/images/about-clicked.png"
+    }
+  ];
+  position = "below";
+
+  hoverIn = function (nav) {
+    console.log(nav);
+    nav.img = nav.srcHover;
+  };
+
+  hoverOut = function (nav) {
+    nav.img = nav.src;
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdTooltipModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
